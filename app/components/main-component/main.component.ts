8b3cb4ed34b6a32e4b12526d92e8bddcd8d14934
Artsiom Aliakseichyk <artsiom_aliakseichyk@epam.com>
2016-12-21 10:16:42 +0300

    remove mock data, added detach skeleton

--- app/components/main-component/main.component.ts
+++ app/components/main-component/main.component.ts
@@ -36,6 +36,8 @@ export class WeatherMainComponent implements OnInit, OnDestroy {
                 private zone: NgZone) {}
 
     ngOnInit() {
+        // this.cd.detach();
+        // setInterval(() => this.getCityList(), 5000);
         this.getCityList();
         this.zoneStableCheck();
     }
@@ -48,25 +50,26 @@ export class WeatherMainComponent implements OnInit, OnDestroy {
 
     getCityList() {
         if (navigator.geolocation) {
-            /*navigator.geolocation.getCurrentPosition((position: Position) => {
+            navigator.geolocation.getCurrentPosition((position: Position) => {
                 let currentLocation: Coords = position.coords;
-                // this.weatherData = this.extractWeatherData = this.WeatherApi.getWeatherData({
-                //     latitude: currentLocation.latitude,
-                //     longitude: currentLocation.longitude
-                // });
-                this.subscription = */this.WeatherApi.getWeatherData({
-                    latitude: /*currentLocation.latitude*/111,
-                    longitude: /*currentLocation.longitude*/111
+                this.weatherData = this.extractWeatherData = this.WeatherApi.getWeatherData({
+                    latitude: currentLocation.latitude,
+                    longitude: currentLocation.longitude
+                });
+                this.subscription = this.WeatherApi.getWeatherData({
+                    latitude: currentLocation.latitude,
+                    longitude: currentLocation.longitude
                 }).subscribe(
                     response => {
                         this.cityDetails = response;
                         this.numOfPages = Math.ceil(this.cityDetails.length / CONSTS.ITEMS_IN_PAGE)
                         this.pagesArr = this.arrayOfPages(this.numOfPages);
-                        // this.MapInit.init(currentLocation);
+                        this.MapInit.init(currentLocation);
                     });
-            /*   }
-            );*/
+               }
+            );
         }
+        // this.cd.markForCheck();
     }
     changePage(page: number) {
         this.currentPage = page;
