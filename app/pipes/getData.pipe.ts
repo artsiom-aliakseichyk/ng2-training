3ea63a6019174cad795a6fca386ee8bf0eec157f
Artsiom Aliakseichyk <artsiom_aliakseichyk@epam.com>
2016-12-27 10:17:45 +0300

    Added ngswitch to change weather icon

--- app/pipes/getData.pipe.ts
+++ app/pipes/getData.pipe.ts
@@ -7,10 +7,10 @@ export class GetData implements PipeTransform {
     transform(obj: weatherIcon, param: string): string {
         if (obj !== undefined && obj !== null) {
             let weather = obj;
-            if (param === "icon") {
-                let iconUrl: string = "http://openweathermap.org/img/w/" + weather[0][param] + ".png";
-                return iconUrl;
-            }
+            // if (param === "icon") {
+                // let iconUrl: string = "http://openweathermap.org/img/w/" + weather[0][param] + ".png";
+                // return iconUrl;
+            // }
             return weather[0][param];
         }
     }
